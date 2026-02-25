1fff036d78056d793614df9f8ac3199a9ee8fcf4
Akanksha <priyaakanksha031@gmail.com>
2026-02-25 16:09:08 +0530

    add project idea card with basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+const projectIdeas = [
+  'weather-app',
+  'calculator',
+  'To-do list',
+  'Tic-tac-toe',
+  'rock-paper-scissors'
+]
 function App(){
   return(
     <>
     <h1 className="text-4xl font-bold text-blue-500">Ship it or Skip it</h1>
+    <div className ="card p-8  bg-white max-w-md border border-gray-500 rounded-lg" >
+
+  <p>Would you like to make a {projectIdeas[0]} ?</p>
+     
+   <button>  Ship it</button>
+   <button>Skip it</button>
+   </div>
+  
     </>
   )
 }
