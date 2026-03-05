1d0a81fecaee475c73bc4296ede9114776e0eb70
Akanksha <priyaakanksha031@gmail.com>
2026-03-05 22:27:16 +0530

    done with css

--- src/App.jsx
+++ src/App.jsx
@@ -39,20 +39,21 @@ function App(){
 bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900">
 
     <h1 className="text-4xl font-bold text-blue-500 mb-18">Ship it or Skip it</h1>
-    <div className ="card text-center p-8  bg-white max-w-md h-57 border border-gray-500 rounded-lg" >
+    <div >
   {projectIndex < projectIdeas.length?(
     <>
-  <p className="mt-4">Would you like to make a {projectIdeas[projectIndex]} ?</p>
+  <p className='mt-4 text-center p-8  bg-white max-w-md h-57 border border-gray-500 rounded-lg'>Would you like to make a {projectIdeas[projectIndex]} ?</p>
    <Button onClick={updateShippedList}>Ship it</Button>
    <Button onClick={updateIndex} >Skip it</Button>
    </>
   ):(
-    <p>You're Done!</p>
+    <p className='flex text-center p-8  bg-white border border-gray-500 rounded-lg'>You're Done!</p>
   )}
    </div>
-   
+   <div className='mt-8 border-1 p-4 bg-white rounded-lg'>
+    <h1>Your shipped list:</h1>
    {shippedList.map(idea =>(<p key={idea}>{idea}</p>))}
-  
+  </div>
     </div>
   )
 }
